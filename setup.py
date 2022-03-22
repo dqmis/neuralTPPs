f040b023d60ad93951c6f8b02ebe262265c0ea3f
Dominykas Šeputis <dom.seputis@gmail.com>
2022-03-22 22:11:38 +0200

    Updated model loading

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='tpp',
-    version='0.0.3',
+    version='0.0.4',
     description='Playing around with TPPs.',
     long_description=readme,
     author='Babylon ML team',
